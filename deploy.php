e708d53c6a56007e250c40e567edb8c901e16fbf
Lucky Motseki <junoir.mklv@gmail.com>
2020-01-31 12:27:23 +0200

    updated with domain and git repo

--- deploy.php
+++ deploy.php
@@ -7,23 +7,33 @@ require 'recipe/laravel.php';
 set('application', 'my_project');
 
 // Project repository
-set('repository', 'git@domain.com:username/repository.git');
+set('repository', 'git@github.com:simphiwehlabisa/dotcomapp.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', true); 
 
+
 // Shared files/dirs between deploys 
-add('shared_files', []);
-add('shared_dirs', []);
+add('shared_files', ['.env']);
+add('shared_dirs', [
+    'storage',
+]);
 
 // Writable dirs by web server 
-add('writable_dirs', []);
+set('writable_mode', 'chmod');
 
+//global php and composer
+set('bin/php', 'php');
+set('bin/composer', 'composer');
+set('composer_options', 'install --verbose --prefer-dist --no-progress --no-interaction --optimize-autoloader');
 
 // Hosts
+set('http_user', 'csdonjwyzd');
 
-host('project.com')
-    ->set('deploy_path', '~/{{application}}');    
+host('csdonline.co.za')
+    ->user('csdonjwyzd')
+    ->port(22)
+    ->set('deploy_path', '/usr/home/csdonjwyzd/public_html');    
     
 // Tasks
 
